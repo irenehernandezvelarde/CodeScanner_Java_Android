8d89001162dfa10c42ce02dda056ea0ac0447ee3
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2023-01-30 16:42:37 +0100

    //Crear MainActivity y añadir un boton y una textview. //Añadir un Intent que al presionar el boton de la MainActivity nos llevará a otra activity. //Crear CodaScannerActivity, añadir los metodos y pedir los permisos necesarios para la camara.

---
+++ app/src/main/java/com/example/codescanner_java_android/CodeScannerActivity.java
@@ -0,0 +1,72 @@
+package com.example.codescanner_java_android;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+public class CodeScannerActivity extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_scanner);
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+        }
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(CodeScannerActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                } else {
+                }
+            });
+}
--- app/src/main/java/com/example/codescanner_java_android/MainActivity.java
+++ app/src/main/java/com/example/codescanner_java_android/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.codescanner_java_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button start = findViewById(R.id.start);
+        Intent intent = new Intent(MainActivity.this, CodeScannerActivity.class);
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
